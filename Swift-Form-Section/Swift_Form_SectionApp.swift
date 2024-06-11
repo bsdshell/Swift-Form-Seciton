99f015098317d3d07957ab91430830c38bc61a73
name <asdfad@asdfasd.com>
2024-06-11 14:45:27 -0700

    Initial Commit

---
+++ Swift-Form-Section/Swift_Form_SectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Form_SectionApp.swift
+//  Swift-Form-Section
+//
+//  Created by aaa on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Form_SectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
